85a90bf38bdf57814c2937a43034e0e1d043e70b
n de gro <algipari304@gmail.com>
2025-02-04 07:46:19 +0700

    Add files via upload

---
+++ contohjs1/contohjs_6.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script type="text/javascript">
+    function myfunction(txt)
+    {
+        alert(txt)
+    }
+    </script>
+</head>
+<body>
+    <form action="">
+        <input type="button" onclick="myfunction('Good Morning!')" value="Selamat Pagi">
+        <input type="button" onclick="myfunction('Good Evening!')" value="Selamat Malam">
+ </form>
+ <p>
+    ketika di tekan salah satu tombol maka fungsi akan di panggil dan pesan akan di tampilkan
+ </p>
+</body>
+</html>
